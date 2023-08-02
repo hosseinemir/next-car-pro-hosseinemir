e1406333337ca05a0c7d10be9fd4210fbfc26e61
hosseinemir <hosseinemir79@gmail.com>
2023-08-02 10:23:01 +0330

    fix name bug

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -7,7 +7,7 @@ import Back from "@/components/icons/Back";
 import Search from "@/components/module/Search/Search";
 import styles from "../../styles/Category.module.css";
 
-export default function category() {
+export default function Categorydynamic() {
   const categorytype = useRouter();
   const { category } = categorytype.query;
   const selectedcars = carsData.filter((car) => car.category === category);
